bb33534be79c55b8264366395e7d351953a6bdcc
Alex Geis <siegxela@gmail.com>
2023-02-21 15:01:46 -0700

    add home and login routes, import into create_app function and confirm on dev server

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from app.routes import home
 from flask import Flask
 
 def create_app(test_config=None):
@@ -11,5 +12,8 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello world'
+
+  # register routes
+  app.register_blueprint(home)
 
   return app
