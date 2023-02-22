58fe00e9e28b7436ba4e53689a613ff84affe164
Alex Geis <siegxela@gmail.com>
2023-02-22 15:00:01 -0700

    add autopep8 python formatter

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,21 @@
 from app.routes import home, dashboard
 from flask import Flask
 
+
 def create_app(test_config=None):
-  # set up app config
-  app = Flask(__name__, static_url_path='/')
-  app.url_map.strict_slashes = False
-  app.config.from_mapping(
-    SECRET_KEY='super_secret_key'
-  )
+    # set up app config
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='super_secret_key'
+    )
 
-  @app.route('/hello')
-  def hello():
-    return 'hello world'
+    @app.route('/hello')
+    def hello():
+        return 'hello world'
 
-  # register routes
-  app.register_blueprint(home)
-  app.register_blueprint(dashboard)
+    # register routes
+    app.register_blueprint(home)
+    app.register_blueprint(dashboard)
 
-  return app
+    return app
--- app/models/User.py
+++ app/models/User.py
@@ -5,23 +5,24 @@ import bcrypt
 
 salt = bcrypt.gensalt()
 
+
 class User(Base):
-  __tablename__ = 'users'
-  id = Column(Integer, primary_key=True)
-  username = Column(String(50), nullable=False)
-  email = Column(String(50), nullable=False, unique=True)
-  password = Column(String(100), nullable=False)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
 
-  @validates('email')
-  def validate_email(self, key, email):
-    # make sure email address contains @ character
-    assert '@' in email
+    @validates('email')
+    def validate_email(self, key, email):
+        # make sure email address contains @ character
+        assert '@' in email
 
-    return email
-  
-  @validates('password')
-  def validate_password(self, key, password):
-    assert len(password) > 4
+        return email
 
-    # encrypt password
-    return bcrypt.hashpw(password.encode('utf-8'), salt)
+    @validates('password')
+    def validate_password(self, key, password):
+        assert len(password) > 4
+
+        # encrypt password
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
