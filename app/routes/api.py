1b0eccaf1d11be782a0eef883700ce6cc01b6573
Alex Geis <siegxela@gmail.com>
2023-02-23 10:55:13 -0700

    update signup method to catch potentials errors and print to the console

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from app.models import User
 from app.db import get_db
 from flask import Blueprint, request, jsonify
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -11,15 +12,21 @@ def signup():
     data = request.get_json()
     db = get_db()
 
-    # create a new user
-    newUser = User(
-        username=data['username'],
-        email=data['email'],
-        password=data['password']
-    )
+    try:
+        # attempt creating a new user
+        newUser = User(
+            username=data['username'],
+            email=data['email'],
+            password=data['password']
+        )
 
-    # save in database
-    db.add(newUser)
-    db.commit()
+        db.add(newUser)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        # insert failed, so rollback and send error to front end
+        db.rollback()
+        return jsonify(message='Signup failed'), 500
 
     return jsonify(id=newUser.id)
