3e94be80b36f0e09e03302284c8845b1ad309f87
Alex Geis <siegxela@gmail.com>
2023-02-23 07:59:45 -0700

    import flask application context global variable and add get_db() method to check against this global variable for a previous version of the db before creating a session

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,8 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+# flask application context global variable "g"
+from flask import g
 
 load_dotenv()
 
@@ -15,3 +17,11 @@ Base = declarative_base()
 
 def init_db():
     Base.metadata.create_all(engine)
+
+
+def get_db():
+    if 'db' not in g:
+        # store db connection in app context
+        g.db = Session()
+
+    return g.db
