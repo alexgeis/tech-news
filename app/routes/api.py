b9e84eef4570ac4c3899e6cb6f10332398b5c60c
Alex Geis <siegxela@gmail.com>
2023-02-23 12:01:39 -0700

    update login route to check for correct password and create new sessions if no errors

--- app/routes/api.py
+++ app/routes/api.py
@@ -48,7 +48,16 @@ def login():
     except:
         print(sys.exc_info()[0])
 
-    return jsonify(message='Incorrect credentials'), 400
+        return jsonify(message='Incorrect credentials'), 400
+
+    if user.verify_password(data['password']) == False:
+        return jsonify(message='Incorrect credentials'), 400
+
+    session.clear()
+    session['user_id'] = user.id
+    session['loggedIn'] = True
+
+    return jsonify(id=user.id)
 
 
 @bp.route('/users/logout', methods=['POST'])
