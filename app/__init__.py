6e35b7ae70bae614d98805b6982085f240967424
Alex Geis <siegxela@gmail.com>
2023-02-23 09:58:07 -0700

    update post-info.html to use filter functions for dates, urls, and plurals

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from app.routes import home, dashboard
 from flask import Flask
 from app.db import init_db
+from app.utils import filters
 
 
 def create_app(test_config=None):
@@ -10,6 +11,10 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='super_secret_key'
     )
+    # formatting filters
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
 
     @app.route('/hello')
     def hello():
