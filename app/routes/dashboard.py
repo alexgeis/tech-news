cea25231f3b4c6ec544e5e651d2c1cbd67845a45
Alex Geis <siegxela@gmail.com>
2023-02-27 11:07:45 -0700

    updated home.py route so post data is passed into user dashboard template

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,11 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
+
 @bp.route('/')
 def dash():
-  return render_template('dashboard.html')
+    db = get_db()
+    posts = (
+        db.query(Post)
+        .filter(Post.user_id == session.get('user_id'))
+        .order_by(Post.created_at.desc())
+        .all()
+    )
+    return render_template(
+        'dashboard.html',
+        posts=posts,
+        loggedIn=session.get('loggedIn')
+    )
+
 
 @bp.route('/edit/<id>')
 def edit(id):
-  return render_template('edit-post.html')
+    return render_template('edit-post.html')
